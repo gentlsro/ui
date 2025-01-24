0fdd978f00f866b394894e5ab6409ed4c7846299
Kylian, Jakub <jacob.kylian@gmail.com>
2025-01-24 17:14:28 +0100

    up

--- index.ts
+++ index.ts
@@ -1,5 +1,17 @@
+/* eslint-disable perfectionist/sort-exports */
+
+// Btn
+export { BUTTON_PRESET } from './client/components/Button/constants/button-preset.constant'
+
+// Colors
+export { useColors } from './shared/composables/useColors'
+
+// List
 export type { IListFetchPayload } from './client/components/List/types/list-fetch.type'
+
+// Notification
 export type { INotification } from './client/components/Notification/types/notification.type'
+
+// Table
 export { TableColumn } from './client/components/Table/models/table-column.model'
 export type { ITableFetchPayload } from './client/components/Table/types/table-fetch-payload.type'
-export { useColors } from './shared/composables/useColors'
