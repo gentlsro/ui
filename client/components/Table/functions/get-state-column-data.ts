70556b632c944d1e04ac2ecefb2d93fc810c93f7
Kylian, Jakub <jacob.kylian@gmail.com>
2025-02-04 21:36:10 +0100

    up

--- client/components/Table/functions/get-state-column-data.ts
+++ client/components/Table/functions/get-state-column-data.ts
@@ -2,7 +2,7 @@
 import type { TableColumn } from '../models/table-column.model'
 
 export function getStateColumnData(column: TableColumn) {
-  const colProps = pick(column, ['field', 'dataType', 'frozen', 'hidden', 'semiFrozen', 'sort', 'sortOrder', 'width'])
+  const colProps = pick(column, ['field', 'frozen', 'hidden', 'semiFrozen', 'sort', 'sortOrder', 'width'])
   const colFilters = column.filters?.map(f => pick(f, ['comparator', 'dataType', 'field', 'filterField', 'id', 'nonInteractive', 'value']))
 
   return { ...colProps, filters: colFilters }
