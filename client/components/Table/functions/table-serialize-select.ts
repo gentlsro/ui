5ffa0118462e7262e598a7732d2c8114be6aedea
Kylian, Jakub <jacob.kylian@gmail.com>
2025-02-04 15:58:50 +0100

    up

--- client/components/Table/functions/table-serialize-select.ts
+++ client/components/Table/functions/table-serialize-select.ts
@@ -9,13 +9,26 @@ import type { TableColumn } from '../models/table-column.model'
 export function tableSerializeSelect(payload: {
   columns?: TableColumn[]
   select?: string[]
-}): string {
-  const { columns = [], select } = payload
 
-  // The select that user sees in the URL
-  const _select = select ?? columns
-    .filter(col => !col.isHelperCol && !col.hidden)
-    .map(col => col.field)
+  /**
+   * When true, the select will also include the columns that are not visible but
+   * are `alwaysSelected` or part of th `needsFields` list
+   */
+  includeInvisible?: boolean
+}): string {
+  const { columns = [], select, includeInvisible } = payload
+
+  let _select: string[]
+
+  if (includeInvisible) {
+    _select = select ?? columns
+      .filter(col => col.alwaysSelected || (!col.isHelperCol && !col.hidden))
+      .flatMap(col => [...(col.local ? [] : [col.field]), ...(col.needsFields ?? [])])
+  } else {
+    _select = select ?? columns
+      .filter(col => !col.isHelperCol && !col.hidden)
+      .map(col => col.field)
+  }
 
   return uniq(_select).join(',')
 }
