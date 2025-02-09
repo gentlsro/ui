440817b53163bf14d04c582d67e62cafaa98c3c6
Kylian, Jakub <jacob.kylian@gmail.com>
2025-02-09 13:47:11 +0100

    up

--- client/components/Table/stores/table.store.ts
+++ client/components/Table/stores/table.store.ts
@@ -259,7 +259,7 @@ export function useTableStore(
 
       // Set the state columns once everything is set
       if (propsColumns.value && apiColumns.value) {
-        // syncStateColumns()
+        syncStateColumns()
       }
     }, { immediate: true })
     // !SECTION
@@ -449,7 +449,7 @@ export function useTableStore(
       // We can assume that the meta is loaded if `apiColumns` is not `undefined`
       // and `propsColumns` is not `undefined`
       if (apiColumns.value && propsColumns.value) {
-        // syncStateColumns()
+        syncStateColumns()
 
         // Load data with new query params
         fetchAndSetData()
