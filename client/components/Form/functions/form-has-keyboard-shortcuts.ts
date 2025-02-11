0df4ac43359a8fe2f02a6c527f05335eee62a6af
Kylian, Jakub <jacob.kylian@gmail.com>
2025-02-11 22:01:48 +0100

    up

--- client/components/Form/functions/form-has-keyboard-shortcuts.ts
+++ client/components/Form/functions/form-has-keyboard-shortcuts.ts
@@ -2,11 +2,9 @@ export function formHasKeyboardShortcuts(
   propsNoShortcuts?: boolean,
   storeNoShortcuts?: boolean,
 ) {
-  console.log('Log ~ storeNoShortcuts:', storeNoShortcuts)
-  console.log('Log ~ propsNoShortcuts:', propsNoShortcuts)
   if (!isNil(propsNoShortcuts)) {
     return !propsNoShortcuts
   }
 
-  return !!storeNoShortcuts
+  return !storeNoShortcuts
 }
