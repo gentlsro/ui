139535fe3471958cf163ebb93b44910909dcd410
Kylian, Jakub <jacob.kylian@gmail.com>
2025-02-13 14:39:14 +0100

    up

--- client/components/Table/functions/table-get-exposed.ts
+++ client/components/Table/functions/table-get-exposed.ts
@@ -1,12 +1,16 @@
+import type { Required } from 'utility-types'
+
 // Functions
 import { useTableAutoFit } from '../composables/useTableAutoFit'
 
 // Store
 import { useTableStore } from '../stores/table.store'
+import { tableSelectRow } from './table-select-row'
 
 export function tableGetExposed() {
   const { fitColumns } = useTableAutoFit()
   const tableStore = useTableStore()
+  const { rowKey, selectionConfig, selection, selectionByKey } = toRefs(tableStore)
 
   return {
     refetch: tableStore.fetchAndSetData,
@@ -14,5 +18,16 @@ export function tableGetExposed() {
     fitColumns,
     getFetchPayload: () => tableStore.getFetchPayload(),
     getVirtualScroller: () => tableStore.virtualScrollEl,
+    selectRow: (payload: Required<Partial<Parameters<typeof tableSelectRow>[0]>, 'row'>) => {
+      const _payload = {
+        ...payload,
+        selection: payload.selection ?? selection,
+        selectionByKey: payload.selectionByKey ?? (selectionByKey as unknown as Record<string, boolean>),
+        selectionConfig: payload.selectionConfig ?? selectionConfig.value,
+        rowKey: payload.rowKey ?? rowKey.value,
+      }
+
+      tableSelectRow(_payload)
+    },
   }
 }
