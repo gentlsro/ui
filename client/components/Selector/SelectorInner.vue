0f44c328ae03faddad312ba48ac8927e4cea63ab
Kylian, Jakub <jacob.kylian@gmail.com>
2025-02-14 18:11:52 +0100

    up

--- client/components/Selector/SelectorInner.vue
+++ client/components/Selector/SelectorInner.vue
@@ -102,8 +102,6 @@ function handleRemove(idx: number) {
       v-for="(item, idx) in modelArray"
       :key="idx"
     >
-      {{ item }}
-      {{ getListItem(item, optionByKey) }}
       <slot
         :item="getListItem(item, optionByKey)"
         :index="idx"
