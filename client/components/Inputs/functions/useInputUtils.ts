5303a6820fd04255173db4ff19576c5159f97543
Kylian, Jakub <jacob.kylian@gmail.com>
2025-02-14 13:48:57 +0100

    up

--- client/components/Inputs/functions/useInputUtils.ts
+++ client/components/Inputs/functions/useInputUtils.ts
@@ -36,8 +36,7 @@ export function useInputUtils(options: IInputUtilsOptions) {
   const lastValidValue = ref<any>()
   const { emptyValue } = toRefs(props)
 
-  console.log(maskRef.value)
-  const { el, mask, masked, unmasked, typed } = useIMask(maskRef.value, {
+  const { el, mask, masked, unmasked, typed } = useIMask(maskRef, {
     onAccept: ev => {
       nextTick(() => {
         const val = maskEventHandlers?.onAccept?.(
