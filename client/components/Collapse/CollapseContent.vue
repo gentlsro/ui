be27e25ecffa5af7c77c761e3e74e86ab93d02a4
Kylian, Jakub <jacob.kylian@gmail.com>
2025-03-10 16:34:27 +0100

    up

--- client/components/Collapse/CollapseContent.vue
+++ client/components/Collapse/CollapseContent.vue
@@ -116,7 +116,7 @@ if (props.autoAdjustHeight) {
 
 <style lang="scss" scoped>
 .collapse__content {
-  @apply flex flex-col overflow-auto rounded-b-custom z-10;
+  @apply flex flex-col overflow-auto rounded-b-custom z-11; // z-11 is above input labels
 
   &.is-floating {
     @apply absolute left-0 right-0 bottom-0 translate-y-full;
