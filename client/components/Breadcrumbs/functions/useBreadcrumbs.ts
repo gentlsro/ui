d56f5dabe710630f976885b8bb9a1ac048b23e45
Kylian, Jakub <jacob.kylian@gmail.com>
2025-03-27 17:17:15 +0100

    up

--- client/components/Breadcrumbs/functions/useBreadcrumbs.ts
+++ client/components/Breadcrumbs/functions/useBreadcrumbs.ts
@@ -8,11 +8,16 @@ import { breadcrumbsKey } from '../provide/breadcrumbs.provide'
 
 export function useBreadcrumbs(
   breadcrumbsRef?: MaybeRefOrGetter<IBreadcrumb[]>,
-  options?: { useLastBreadcrumbAsTitle?: boolean, title?: MaybeRefOrGetter<string> },
+  options?: {
+    useLastBreadcrumbAsTitle?: boolean
+    title?: MaybeRefOrGetter<string>
+    trigger?: MaybeRefOrGetter<any>
+  },
 ) {
   const {
     useLastBreadcrumbAsTitle = uiConfig.breadcrumbs.misc.useLastBreadcrumbAsTitle,
     title,
+    trigger,
   } = options ?? {}
 
   const injectedBreadcrumbs = injectLocal(breadcrumbsKey, ref([])) as Ref<IBreadcrumb[]>
@@ -21,20 +26,27 @@ export function useBreadcrumbs(
     injectedBreadcrumbs.value = toValue(breadcrumbsRef)
   }
 
-  if (injectedBreadcrumbs.value && title) {
-    useHead({ title })
-  } else if (injectedBreadcrumbs.value && useLastBreadcrumbAsTitle) {
-    const lastBreadcrumb = toValue(injectedBreadcrumbs.value).at(-1)
-    const title = typeof lastBreadcrumb?.label === 'function'
-      ? lastBreadcrumb.label()
-      : lastBreadcrumb?.label
+  function setTitle() {
+    if (injectedBreadcrumbs.value && title) {
+      useHead({ title })
+    } else if (injectedBreadcrumbs.value && useLastBreadcrumbAsTitle) {
+      const lastBreadcrumb = toValue(injectedBreadcrumbs.value).at(-1)
 
-    if (title) {
-      useHead({ title: String(title) })
+      const title = typeof lastBreadcrumb?.label === 'function'
+        ? lastBreadcrumb.label()
+        : lastBreadcrumb?.label
+
+      if (title) {
+        useHead({ title: String(title) })
+      }
     }
   }
 
   provideLocal(breadcrumbsKey, injectedBreadcrumbs)
 
-  return { breadcrumbs: injectedBreadcrumbs }
+  setTitle()
+
+  watch(trigger, setTitle)
+
+  return { breadcrumbs: injectedBreadcrumbs, refresh: setTitle }
 }
