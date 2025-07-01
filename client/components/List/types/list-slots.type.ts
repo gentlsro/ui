62cf5aa77929432a61e5098942e2da9bfe60f447
Kylian, Jakub <jacob.kylian@gmail.com>
2025-07-01 18:56:55 +0200

    up

--- client/components/List/types/list-slots.type.ts
+++ client/components/List/types/list-slots.type.ts
@@ -7,7 +7,7 @@ import type { IListProps } from './list-props.type'
 export type IListSlots = {
   'search': (props: { ui?: IListProps['ui'] }) => any
   'content': (props: { ui?: IListProps['ui'], scrollerConfig?: IListProps['scrollerConfig'] }) => any
-  'item'?: (props: { row: IListItem, index: number, isDisabled: boolean, isSelected: boolean }) => any
+  'item'?: (props: { row: IListItem, index: number, isDisabled?: boolean, isSelected?: boolean }) => any
   'noData'?: () => any
   'loading'?: () => any
   'move-handle'?: () => any
--- client/components/Tree/types/tree-emits.type.ts
+++ client/components/Tree/types/tree-emits.type.ts
@@ -1,4 +1,5 @@
 export type ITreeEmits = {
   (e: 'click:node', payload: { node: ITreeNode, ev?: MouseEvent }): void
   (e: 'focus:node', payload: { node: ITreeNode }): void
+  (e: 'blur:node', payload: { node?: ITreeNode }): void
 }
