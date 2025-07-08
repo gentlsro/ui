a8a18ab493f7e9cc9dd6154d2a61faafe441895d
Kylian, Jakub <jacob.kylian@gmail.com>
2025-07-08 18:45:05 +0200

    up

--- client/components/Breadcrumbs/types/breadcrumbs-props.type.ts
+++ client/components/Breadcrumbs/types/breadcrumbs-props.type.ts
@@ -1,8 +1,22 @@
 import type { CSSProperties } from 'vue'
+import type { IBreadcrumb } from '~~/libs/UI/client/components/Breadcrumbs/types/breadcrumb.type'
 
 export type IBreadcrumbsProps = {
+  /**
+   * Path to the home page
+   */
   homePath?: string | (() => string)
 
+  /**
+   * Breadcrumbs
+   *
+   * NOTE: These will take priority over `useBreadcrumbs`
+   */
+  breadcrumbs?: IBreadcrumb[]
+
+  /**
+   * Visual configuration
+   */
   ui?: {
     /**
      * Class to apply to the breadcrumbs wrapper
