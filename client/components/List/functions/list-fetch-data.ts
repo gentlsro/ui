928907981e94d651e1938b169ef949bd8ddf95e7
Kylian, Jakub <jacob.kylian@gmail.com>
2025-07-29 14:00:09 +0200

    up

--- client/components/List/functions/list-fetch-data.ts
+++ client/components/List/functions/list-fetch-data.ts
@@ -47,8 +47,7 @@ export async function listFetchData(payload: {
       return false
     }
 
-    // @ts-expect-error
-    return !item._isNew && !item._isCreate
+    return !item.ref._isNew && !item.ref._isCreate
   })
   const res = await handleRequest<IItem>(abortController => {
     return fnc!({
