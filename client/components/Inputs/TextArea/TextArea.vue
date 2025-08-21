39d28ebd9bf5fe11370e0662f5370218727d0d05
Kylian, Jakub <jacob.kylian@gmail.com>
2025-08-21 14:08:14 +0200

    up

--- client/components/Inputs/TextArea/TextArea.vue
+++ client/components/Inputs/TextArea/TextArea.vue
@@ -130,7 +130,7 @@ defineExpose({
           v-if="hasClearableBtn"
           :clear-confirmation
           :size
-          class="self-start m-t-2"
+          class="self-start m-t-1.5"
           @click.stop.prevent="!clearConfirmation && clear()"
         />
       </div>
