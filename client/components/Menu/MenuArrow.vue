965e7196f2dab2f402244dc0930f9aa197282acc
Kylian, Jakub <jacob.kylian@gmail.com>
2025-09-15 16:33:13 +0200

    up

--- client/components/Menu/MenuArrow.vue
+++ client/components/Menu/MenuArrow.vue
@@ -21,28 +21,28 @@ const { arrowEl } = useMenuStore()
 // Arrow placement
 .menu[placement^='top'] > .menu-arrow {
   @apply bottom--4px shadow-darker/12 shadow-light/8;
-  // @apply border-b-custom border-r-custom border-ca;
+  @apply border-b-custom border-r-custom border-ca;
 
   box-shadow: 1px 1px 1px 1px var(--un-shadow-color);
 }
 
 .menu[placement^='bottom'] > .menu-arrow {
   @apply top--4px shadow-darker/12 shadow-light/8;
-  // @apply border-t-custom border-l-custom border-ca;
+  @apply border-t-custom border-l-custom border-ca;
 
   box-shadow: -1px -1px 1px 1px var(--un-shadow-color);
 }
 
 .menu[placement^='left'] > .menu-arrow {
   @apply right--4px shadow-darker/12 shadow-light/8;
-  // @apply border-r-custom border-t-custom border-ca;
+  @apply border-r-custom border-t-custom border-ca;
 
   box-shadow: 1px -1px 1px 1px var(--un-shadow-color);
 }
 
 .menu[placement^='right'] > .menu-arrow {
   @apply left--4px shadow-darker/12 shadow-light/8;
-  // @apply border-l-custom border-b-custom border-ca;
+  @apply border-l-custom border-b-custom border-ca;
 
   box-shadow: -1px 1px 1px 1px var(--un-shadow-color);
 }
