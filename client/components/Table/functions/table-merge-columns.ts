447db7c59ed3f70a2a5644078697da8274154caa
Kylian, Jakub <jacob.kylian@gmail.com>
2025-12-04 16:28:17 +0100

    up

--- client/components/Table/functions/table-merge-columns.ts
+++ client/components/Table/functions/table-merge-columns.ts
@@ -29,7 +29,7 @@ export function tableMergeColumns(payload: {
 
     // Make sure to remove columns that are only in the state but not in any other sources
     // But only if there actually are some api columns
-    if (apiColumns.length && !apiCol && !propsCol) {
+    if ((apiColumns.length && !apiCol && !propsCol) || (propsColumns.length && !apiCol)) {
       return null
     }
 
@@ -41,7 +41,6 @@ export function tableMergeColumns(payload: {
 
     // Merge the column objects, with given priority: state > props > api
     const col = merge({}, apiCol, propsCol, stateCol) as TableColumn<any>
-
 
     col.misc = {
       ...col.misc,
