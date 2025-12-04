1525343da4ba15f82d031019729203690553c7de
Kylian, Jakub <jacob.kylian@gmail.com>
2025-12-04 19:33:15 +0100

    up

--- client/components/Table/functions/table-merge-columns.ts
+++ client/components/Table/functions/table-merge-columns.ts
@@ -35,7 +35,7 @@ export function tableMergeColumns(payload: {
 
     // Make sure to remove columns that are only in the state but not in any other sources
     // But only if there actually are some api columns
-    if (!apiCol && !propsCol) {
+    if (apiColumns.length && !apiCol && !propsCol) {
       return null
     }
 
