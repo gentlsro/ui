91a44ef1bed8b57a87c1dd2f195586099c61d458
Kylian, Jakub <jacob.kylian@gmail.com>
2026-03-16 14:45:53 +0100

    up

--- client/components/Table/Table.vue
+++ client/components/Table/Table.vue
@@ -245,7 +245,7 @@ onMounted(() => {
         :key="col.name"
         #[col.name]="{ row, column, index, value }"
       >
-        {{ $log(row) }}
+        Row. {{ $log(row) }}
         <!-- <slot
           v-if="row"
           :name="col.name"
