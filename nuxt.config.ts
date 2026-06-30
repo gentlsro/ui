03fa05dfb6c2270463934234a89b19d590271cb1
Kylian, Jakub <jacob.kylian@gmail.com>
2026-06-30 13:42:38 +0200

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,16 +4,15 @@ import { createResolver } from 'nuxt/kit'
 import { writeFile } from 'node:fs/promises'
 import { cwd } from 'node:process'
 
-// Constants
-import { gentlUIPreset } from './app/constants/unocss-preset'
-
 const isMonorepo = import.meta.env.VITE_MONOREPO === 'true'
 const isInstallLayerDeps = true
 
 const { resolve } = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
-  extends: ['../Utilities'],
+  extends: isMonorepo
+    ? [['../Utilities']]
+    : [['github:gentlsro/Utilities#v2.2', { install: isInstallLayerDeps }]],
 
   modules: [
     '@nuxtjs/i18n',
