30de47eb5b4818ab736a8e223208e173279b6c41
Kylian, Jakub <jacob.kylian@gmail.com>
2026-07-06 12:27:02 +0200

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,10 +35,6 @@ export default defineNuxtConfig({
     ],
   },
 
-  future: {
-    compatibilityVersion: 5,
-  },
-
   imports: {
     imports: [
       { name: 'useUIStore', from: resolve('./app/stores/ui.store.ts') },
@@ -85,6 +81,10 @@ export default defineNuxtConfig({
   build: {
     // No touchy or I cut fingers
     transpile: ['imask', 'vue-imask'],
+  },
+
+  future: {
+    compatibilityVersion: 5,
   },
 
   vite: {
