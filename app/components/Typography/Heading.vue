2cfb0ddcd571cef26acb8c40ef47ffaa308e21f6
Kylian, Jakub <jacob.kylian@gmail.com>
2026-08-07 16:35:15 +0200

    up

--- app/components/Typography/Heading.vue
+++ app/components/Typography/Heading.vue
@@ -45,7 +45,7 @@ const containerStyle = computed(() => {
 <style lang="scss" scoped>
 .heading.is-highlighted {
   &::before {
-    @apply bottom-0 w-9/10 h-1 left-0 bg-primary content-empty absolute
+    @apply bottom--2 w-9/10 h-1 left-0 bg-primary content-empty absolute
       rounded-full
       bg-gradient-to-r from-primary to-true-gray-100;
   }
